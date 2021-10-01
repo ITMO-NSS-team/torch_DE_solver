47240cb6931048e3835c8666c4532766c05ee1dc
SuperSashka <heretik.unlimited@gmail.com>
2021-10-01 19:36:33 +0300

    KdV cache

--- examples/example_KdV.py
+++ examples/example_KdV.py
@@ -258,7 +258,7 @@ for _ in range(1):
 
     start = time.time()
     model = point_sort_shift_solver(grid, model, kdv, bconds, lambda_bound=100,verbose=2, learning_rate=1e-4,
-                                    eps=1e-7, tmin=1000, tmax=1e5, h=0.01,use_cache=False,cache_verbose=True,
+                                    eps=1e-7, tmin=1000, tmax=1e5, h=0.01,use_cache=True,cache_verbose=True,
                                 batch_size=None, save_always=True)
     # model = point_sort_shift_solver(grid, model, kdv, bconds, lambda_bound=1000,verbose=True, learning_rate=1e-4,
     #                                 eps=1e-6, tmin=1000, tmax=1e5, h=0.01,use_cache=True,cache_verbose=True,
