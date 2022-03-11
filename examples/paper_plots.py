88812e0ef4f53e8402b267a6b65054d338f5232e
SuperSashka <heretik.unlimited@gmail.com>
2022-03-11 18:56:55 +0300

    Plots update

--- examples/paper_plots.py
+++ examples/paper_plots.py
@@ -11,7 +11,7 @@ import seaborn as sns
 
 
 
-sns.set(rc={'figure.figsize':(11.7,8.27)},font_scale=3)
+sns.set(rc={'figure.figsize':(11.7,8.27)},font_scale=4)
 sns.set_style("whitegrid")
 
 df1=pd.read_csv('benchmarking_data/legendre_poly_exp_cache=False.csv',index_col=0)
@@ -19,30 +19,45 @@ df2=pd.read_csv('benchmarking_data/legendre_poly_exp_cache=True.csv',index_col=0
 df_L=pd.concat((df1,df2))
 
 
-plt.figure()
-sns.boxplot(x='type', y='RMSE', data=df_L, showfliers=False, hue='cache')
+# plt.figure()
+# sns.boxplot(x='type', y='RMSE', data=df_L, showfliers=False, hue='cache')
 
 
-plt.figure()
-sns.boxplot(x='type', y='time', data=df_L, showfliers=False, hue='cache')
+# plt.figure()
+# sns.boxplot(x='type', y='time', data=df_L, showfliers=False, hue='cache')
 
 
-plt.figure()
+
+fig, axes = plt.subplots(1, 2, figsize=(40,20))
+
+sns.boxplot(ax=axes[0],x='type', y='time', data=df_L, showfliers=False, hue='cache')
+
+sns.boxplot(ax=axes[1],x='type', y='RMSE', data=df_L, showfliers=False, hue='cache')
+
+
+
+plt.savefig('Legendre_exps_NN.eps', format='eps',bbox_inches='tight')
+
+
+
 df_L_mat=pd.read_csv('benchmarking_data/legendre_poly_exp_martix.csv',index_col=0)
 df_L_mat['method']='matrix'
 df2['method']='NN'
 df_mat_compare=pd.concat((df2,df_L_mat))
 
-sns.boxplot(x='type', y='time', data=df_mat_compare, showfliers=False, hue='method')
-plt.figure()
-sns.boxplot(x='type', y='RMSE', data=df_mat_compare, showfliers=False, hue='method')
 
-plt.figure()
-sns.boxplot(x='type', y='RMSE', data=df_L, showfliers=False, hue='cache')
+# plt.figure()
+# sns.boxplot(x='type', y='time', data=df_mat_compare, showfliers=False, hue='method')
+# plt.figure()
+# sns.boxplot(x='type', y='RMSE', data=df_mat_compare, showfliers=False, hue='method')
 
+fig, axes = plt.subplots(1, 2, figsize=(40,20))
 
-plt.figure()
-sns.boxplot(x='type', y='time', data=df_L, showfliers=False, hue='cache')
+sns.boxplot(ax=axes[0],x='type', y='time', data=df_mat_compare, showfliers=False, hue='method')
+
+sns.boxplot(ax=axes[1],x='type', y='RMSE', data=df_mat_compare, showfliers=False, hue='method')
+
+plt.savefig('Legendre_exps_NN_vs_mat.eps', format='eps',bbox_inches='tight')
 
 
 
@@ -90,35 +105,35 @@ plt.savefig('PI-PIII_exps.eps', format='eps',bbox_inches='tight')
 
 dfPIV=pd.read_csv('benchmarking_data/PIV_experiment_100_500_cache=True.csv',index_col=0)
 
-plt.figure()
+# plt.figure()
 
-sns.scatterplot(x='grid_res', y='RMSE', data=dfPIV, hue='cache')
+# sns.scatterplot(x='grid_res', y='RMSE', data=dfPIV, hue='cache')
 
-plt.figure()
+# plt.figure()
 
-sns.scatterplot(x='grid_res', y='time', data=dfPIV, hue='cache')
+# sns.scatterplot(x='grid_res', y='time', data=dfPIV, hue='cache')
 
 
 dfPV=pd.read_csv('benchmarking_data/PV_experiment_100_500_cache=True.csv',index_col=0)
 
-plt.figure()
+# plt.figure()
 
-sns.scatterplot(x='grid_res', y='RMSE', data=dfPV, hue='cache')
+# sns.scatterplot(x='grid_res', y='RMSE', data=dfPV, hue='cache')
 
-plt.figure()
+# plt.figure()
 
-sns.scatterplot(x='grid_res', y='time', data=dfPV, hue='cache')
+# sns.scatterplot(x='grid_res', y='time', data=dfPV, hue='cache')
 
 
 dfPVI=pd.read_csv('benchmarking_data/PVI_experiment_100_500_cache=True.csv',index_col=0)
 
-plt.figure()
+# plt.figure()
 
-sns.scatterplot(x='grid_res', y='RMSE', data=dfPVI, hue='cache')
+# sns.scatterplot(x='grid_res', y='RMSE', data=dfPVI, hue='cache')
 
-plt.figure()
+# plt.figure()
 
-sns.scatterplot(x='grid_res', y='time', data=dfPVI, hue='cache')
+# sns.scatterplot(x='grid_res', y='time', data=dfPVI, hue='cache')
 
 
 dfPanleve=pd.concat((dfPI,dfPII,dfPIII,dfPIV,dfPV,dfPVI))
@@ -126,8 +141,6 @@ dfPanleve=dfPanleve.drop(columns='index')
 dfPanleve=dfPanleve.reset_index(drop=True)
 
 
-sns.set(rc={'figure.figsize':(11.7,8.27)},font_scale=2)
-sns.set_style("whitegrid")
 
 compare_list=[]
 
@@ -139,17 +152,31 @@ for grid_res in range(100,501,100):
 
 df_compare=pd.DataFrame(compare_list)
 
-plt.figure()
+# plt.figure()
 
-ax=sns.lineplot(x='grid_res', y='mean_RMSE', data=df_compare, hue='type')
-ax.set(xlabel='grid_res', ylabel='mean(RMSE)')
+# ax=sns.lineplot(x='grid_res', y='mean_RMSE', data=df_compare, hue='type')
+# ax.set(xlabel='grid_res', ylabel='mean(RMSE)')
 
-plt.figure()
+# plt.figure()
 
-ax=sns.lineplot(x='grid_res', y='mean_time', data=df_compare, hue='type')
-ax.set(xlabel='grid_res', ylabel='log(time)')
-plt.show()
+# ax=sns.lineplot(x='grid_res', y='mean_time', data=df_compare, hue='type')
+# ax.set(xlabel='grid_res', ylabel='log(time)')
+# plt.show()
 
+
+fig, axes = plt.subplots(1, 2, figsize=(40,20))
+
+
+sns.lineplot(ax=axes[0],x='grid_res', y='mean_time', data=df_compare, hue='type')
+axes[0].set(xlabel='grid_res', ylabel='log(time)')
+
+
+sns.lineplot(ax=axes[1],x='grid_res', y='mean_RMSE', data=df_compare, hue='type')
+axes[1].set(xlabel='grid_res', ylabel='mean(RMSE)')
+
+
+
+plt.savefig('Panleve_exps_full_mean.eps', format='eps',bbox_inches='tight')
 
 fig, axes = plt.subplots(2, 3, figsize=(60,30))
 
