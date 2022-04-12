Matrix derivative fix

metrics.py
@@ -261,7 +261,7 @@ def derivative_1d(model,grid):
     # du_forward = (u-torch.roll(u, -1)) / (x-torch.roll(x, -1))
     
     # du_backward = (torch.roll(u, 1) - u) / (torch.roll(x, 1) - x)
-    du = (1 / 2) * (torch.roll(u, 1) - torch.roll(u, -1))/(torch.roll(x, 1)-torch.roll(x, -1))
+    du =  (torch.roll(u, 1) - torch.roll(u, -1))/(torch.roll(x, 1)-torch.roll(x, -1))
     du[0] = (u[0]-u[1])/(x[0]-x[1])
     du[-1] = (u[-1]-u[-2])/(x[-1]-x[-2])
     
@@ -275,7 +275,7 @@ def derivative(u_tensor, h_tensor, axis, scheme_order=1, boundary_order=1):
     if (u_tensor.shape[0]==1):
         du=derivative_1d(u_tensor,h_tensor)
         return du
-    
+
     u_tensor = torch.transpose(u_tensor, 0, axis)
     h_tensor = torch.transpose(h_tensor, 0, axis)
     
@@ -373,7 +373,6 @@ def derivative(u_tensor, h_tensor, axis, scheme_order=1, boundary_order=1):
             du[:, -1] = du_backward[:, -1]
             
     du = torch.transpose(du, 0, axis)
-    
 
     return du
 
@@ -413,7 +412,6 @@ def take_matrix_diff_term(model, grid, term):
                 h = grid[axis]
                 prod=derivative(prod, h, axis, scheme_order=1, boundary_order=1)
         der_term = der_term * prod ** power[j]
-
     if callable(coeff) is True:
         der_term = coeff(grid) * der_term
     else:

solver.py
@@ -615,13 +615,16 @@ def grid_format_prepare(coord_list, mode='NN'):
         print('Grid is a tensor, assuming old format, no action performed')
         return coord_list
     if mode=='NN':
-        coord_list=torch.tensor(coord_list)
+        # coord_list=torch.tensor(coord_list)
         grid=torch.cartesian_prod(*coord_list).float()
-        grid=grid.reshape(-1,1)
+        # grid=grid.reshape(-1,1)
     elif mode=='mat':
         grid = np.meshgrid(*coord_list)
         grid = torch.tensor(grid)
     return grid
+
+
+
 
 
 