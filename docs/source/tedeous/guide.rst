344552b0bcd3982fb07b43ccb49b98aa33595c6d
Nikita Demyanchuk <jfafterglow@yandex.ru>
2022-12-20 23:29:17 +0300

    docs

--- docs/source/tedeous/guide.rst
+++ docs/source/tedeous/guide.rst
@@ -1,4 +1,3 @@
-Quick guide
+Quickstart
 ===========
 
-TBA...
---
+++ tedeous/device.py
@@ -0,0 +1,13 @@
+import torch
+
+def set_device(*args):
+    if len(args) == 0:
+        if torch.has_cuda:
+            return torch.device('cuda')
+        elif torch.has_mps:
+            return torch.device('mps')
+        else:
+            return torch.device('cpu')
+    if len(args) == 1:
+        device = args[0]
+        return torch.device(device)
