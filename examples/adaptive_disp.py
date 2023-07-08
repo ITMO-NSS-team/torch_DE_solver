cc57971c5fea674e58a0e851d3f3a2c5610ead13
SuperSashka <heretik.unlimited@gmail.com>
2023-07-08 12:59:33 +0300

    First working version

--- examples/adaptive_disp.py
+++ examples/adaptive_disp.py
@@ -1,6 +1,11 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from SALib import ProblemSpec
+
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
 x_grid=np.linspace(0,1,10+1)
 t_grid=np.linspace(0,1,10+1)
@@ -120,9 +125,10 @@ def take_initial_op(model,bnd1,bnd2):
     ic_val=torch.cat((ic_val1,ic_val2))
     return ic_val-ic_true_val
 
-lam=100
+lam_ic=100
+lam_bc=100
 
-loss = torch.mean(take_wave_op(model,grid)**2)+lam*torch.mean(torch.abs(take_boundary_op(model,bc_bnd)))+lam*torch.mean(torch.abs(take_initial_op(model,bnd1,bnd2)))
+loss = torch.mean(take_wave_op(model,grid)**2)+lam_bc*torch.mean(torch.abs(take_boundary_op(model,bc_bnd)))+lam_ic*torch.mean(torch.abs(take_initial_op(model,bnd1,bnd2)))
 
 print(loss.item())
 
@@ -131,25 +137,94 @@ t=0
 loss_mean=1000
 min_loss=np.inf
 
-while loss.item()>1e-5 and t<1e5:
+#while loss.item()>1e-5 and t<1e5:
 
+op_list=[]
+bc_list=[]
+ic_list=[]
+loss_list=[]
+
+PLOT_SAVE=True
+sampling_N=2
+sampling_D=len(grid)+len(bc_bnd)+len(bnd1)+len(bnd2)
+
+second_order_interactions=False
+
+if second_order_interactions:
+    sampling_amount=sampling_N*(2*sampling_D+2)
+else:
+    sampling_amount=sampling_N*(sampling_D+2)
+
+while t<1e5:
         optimizer.zero_grad()
 
         # in case you wanted a semi-full example
         # outputs = model.forward(batch_x)
-        loss = torch.mean(take_wave_op(model,grid)**2)+lam*torch.mean(torch.abs(take_boundary_op(model,bc_bnd)))+lam*torch.mean(torch.abs(take_initial_op(model,bnd1,bnd2)))
+
+        op=take_wave_op(model,grid)
+        bc=take_boundary_op(model,bc_bnd)
+        ic=take_initial_op(model,bnd1,bnd2)
+
+        op_list.append(op.detach().numpy())
+        bc_list.append(bc.reshape(-1).detach().numpy())
+        ic_list.append(ic.reshape(-1).detach().numpy())
+
+        loss = torch.mean(op**2)+lam_bc*torch.mean(torch.abs(bc))+lam_ic*torch.mean(torch.abs(ic))
+
+        loss_list.append(float(loss.item()))
+
         loss.backward()
         optimizer.step()
-        if t%1000==0: 
+
+        if (len(op_list))==sampling_amount:
+        #if (len(op_list))==400:
+            op_array=np.array(op_list)
+            bc_array=np.array(bc_list)
+            ic_array=np.array(ic_list)
+
+            X_array=np.hstack((op_array,bc_array,ic_array))
+
+            loss_array=np.array(loss_list)
+
+
+            bounds=[[-100,100] for i in range(sampling_D)]
+            names=['x{}'.format(i) for i in range(sampling_D)]
+            sp=ProblemSpec({'names':names,'bounds':bounds})
+            sp.set_samples(X_array)
+            sp.set_results(loss_array)
+            sp.analyze_sobol(calc_second_order=second_order_interactions)
+
+            ST=sp.analysis['ST']
+
+            total_disp=sum(ST)
+            bc_disp=sum(ST[len(grid):len(grid)+len(bc_bnd)])
+            ic_disp=sum(ST[len(grid)+len(bc_bnd):])
+
+            lam_bc=total_disp/bc_disp
+            lam_ic=total_disp/ic_disp
+
+            print('Lambda update t={}, loss={}'.format(t,loss.item()))
+            print('New lam_bc={} lam_ic={}'.format(lam_bc,lam_ic))
+
+            op_list=[]
+            bc_list=[]
+            ic_list=[]
+            loss_list=[]
+            
+
+
+        if t%1000==0:
             print('Surface trainig t={}, loss={}'.format(t,loss.item()))
-            fig = plt.figure(figsize=(15,8))
-            ax1 = fig.add_subplot( projection='3d')
-            ax1.plot_trisurf(grid[:, 0].detach().cpu().numpy().reshape(-1), 
-                        grid[:, 1].detach().cpu().numpy().reshape(-1),
-                        model(grid).detach().cpu().numpy().reshape(-1),
-                        cmap=plt.cm.jet, linewidth=0.2, alpha=1)
-            ax1.set_xlabel("x1")
-            ax1.set_ylabel("x2")
-            plt.savefig('disp_exp/plot_disp_{}.png'.format(t))
+            
+            if PLOT_SAVE:
+                fig = plt.figure(figsize=(15,8))
+                ax1 = fig.add_subplot( projection='3d')
+                ax1.plot_trisurf(grid[:, 0].detach().cpu().numpy().reshape(-1), 
+                            grid[:, 1].detach().cpu().numpy().reshape(-1),
+                            model(grid).detach().cpu().numpy().reshape(-1),
+                            cmap=plt.cm.jet, linewidth=0.2, alpha=1)
+                ax1.set_xlabel("x1")
+                ax1.set_ylabel("x2")
+                plt.savefig('disp_exp/plot_disp_{}.png'.format(t))
         t+=1
 
