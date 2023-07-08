a94f0c87c9d1511a780c1ce0ba048e3889c350fe
SuperSashka <heretik.unlimited@gmail.com>
2023-07-09 00:08:29 +0300

    init ODE

--- examples/adaptive_disp_ODE.py
+++ examples/adaptive_disp_ODE.py
@@ -7,28 +7,26 @@ import matplotlib.pyplot as plt
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
-x_grid=np.linspace(0,1,10+1)
-t_grid=np.linspace(0,1,10+1)
-    
-x = torch.from_numpy(x_grid)
-t = torch.from_numpy(t_grid)
-    
-#h=abs((t[1]-t[0]).item())
+t_grid = np.linspace(0, 0.25, 100)
 
-grid = torch.cartesian_prod(x, t).float()
+coord_list = [t_grid]
+
+coord_list=torch.tensor(coord_list)
+grid=coord_list.reshape(-1,1).float()
+    
 
 def func(grid):
-    x, t = grid[:,0],grid[:,1]
-    sln=torch.cos(2*np.pi*t)*torch.sin(np.pi*x)
+    t = grid
+    sln=1/324*(200 - 567*torch.exp(t) + 43*torch.exp(9*t) + 180*t)
     return sln    
 
 # Initial conditions at t=0
-bnd1 = torch.cartesian_prod(x, torch.from_numpy(np.array([0], dtype=np.float64))).float()
+bnd1 = torch.from_numpy(np.array([[0]], dtype=np.float64)).float()
 
 # u(0,x)=sin(pi*x)
-bndval1 = torch.sin(np.pi * bnd1[:, 0])
+bndval1 = torch.tensor([-1]).float()
 
-bnd2 = torch.cartesian_prod(x, torch.from_numpy(np.array([0], dtype=np.float64))).float()
+bnd2 = torch.from_numpy(np.array([[0]], dtype=np.float64)).float()
 
 bop2 = {
 'du/dt':
@@ -40,36 +38,7 @@ bop2 = {
     }
     }
 
-bndval2=torch.from_numpy(np.zeros(len(bnd2), dtype=np.float64))
-
-# Boundary conditions at x=0
-bnd3 = torch.cartesian_prod(torch.from_numpy(np.array([0], dtype=np.float64)), t).float()
-
-# u(0,t)=0
-bndval3 = torch.from_numpy(np.zeros(len(bnd3), dtype=np.float64))
-
-# Boundary conditions at x=1
-bnd4 = torch.cartesian_prod(torch.from_numpy(np.array([1], dtype=np.float64)), t).float()
-
-# u(1,t)=0
-bndval4 = torch.from_numpy(np.zeros(len(bnd4), dtype=np.float64))
-
-# Putting all bconds together
-bconds = [[bnd1, bndval1], [bnd2, bndval2], [bnd3, bndval3], [bnd4, bndval4]]
-
-
-model = torch.nn.Sequential(
-        torch.nn.Linear(2, 100),
-        torch.nn.Tanh(),
-        torch.nn.Linear(100, 100),
-        torch.nn.Tanh(),
-        torch.nn.Linear(100, 100),
-        torch.nn.Tanh(),
-        torch.nn.Linear(100, 100),
-        torch.nn.Tanh(),
-        torch.nn.Linear(100, 1)
-    )
-
+bndval2=torch.tensor([0]).float()
 
 
 def nn_autograd_simple(model, points, order,axis=0):
@@ -102,6 +71,18 @@ def nn_autograd(*args,axis=0):
     return grads.reshape(-1,1)
 
 
+model = torch.nn.Sequential(
+        torch.nn.Linear(1, 100),
+        torch.nn.Tanh(),
+        torch.nn.Linear(100, 100),
+        torch.nn.Tanh(),
+        torch.nn.Linear(100, 100),
+        torch.nn.Tanh(),
+        torch.nn.Linear(100, 100),
+        torch.nn.Tanh(),
+        torch.nn.Linear(100, 1)
+    )
+
 
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
@@ -110,29 +91,28 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 
 
-def take_wave_op(model,grid):
-    return nn_autograd_simple(model, grid, 2,axis=0)-1/4*nn_autograd_simple(model, grid, 2,axis=1)
+def take_ODE_op(model,grid):
+    return nn_autograd_simple(model, grid, 2,axis=0).reshape(-1,1)-10*nn_autograd_simple(model, grid, 1,axis=0).reshape(-1,1)+9*model(grid)-5*grid
 
 
-bc_bnd=torch.cat((bnd3,bnd4))
-bc_true_val=torch.cat((bndval3,bndval4)).reshape(-1,1)
+bc_bnd=bnd1
+bc_true_val=bndval1
 
 def take_boundary_op(model,boundary):
     return model(boundary)-bc_true_val
 
 
-ic_true_val=torch.cat((bndval1,bndval2)).reshape(-1,1)
+ic_true_val=bndval1
+ic_bnd=bnd2
 
-def take_initial_op(model,bnd1,bnd2):
-    ic_val1=model(bnd1)
-    ic_val2=nn_autograd_simple(model, bnd2, 1,axis=1).reshape(-1,1)
-    ic_val=torch.cat((ic_val1,ic_val2))
+def take_initial_op(model,ic_bnd):
+    ic_val=nn_autograd_simple(model, ic_bnd,1,axis=0)
     return ic_val-ic_true_val
 
 lam_ic=10#100
 lam_bc=10#100
 
-loss = torch.mean(take_wave_op(model,grid)**2)+lam_bc*torch.mean(torch.abs(take_boundary_op(model,bc_bnd)))+lam_ic*torch.mean(torch.abs(take_initial_op(model,bnd1,bnd2)))
+loss = torch.mean(take_ODE_op(model,grid)**2)+lam_bc*torch.mean(torch.abs(take_boundary_op(model,bc_bnd)))+lam_ic*torch.mean(torch.abs(take_initial_op(model,ic_bnd)))
 
 print(loss.item())
 
@@ -150,7 +130,7 @@ loss_list=[]
 
 PLOT_SAVE=True
 sampling_N=2
-sampling_D=len(grid)+len(bc_bnd)+len(bnd1)+len(bnd2)
+sampling_D=len(grid)+len(bc_bnd)+len(ic_bnd)
 
 second_order_interactions=True
 
@@ -171,9 +151,9 @@ while t<1e5:
         # in case you wanted a semi-full example
         # outputs = model.forward(batch_x)
 
-        op=take_wave_op(model,grid)
+        op=take_ODE_op(model,grid)
         bc=take_boundary_op(model,bc_bnd)
-        ic=take_initial_op(model,bnd1,bnd2)
+        ic=take_initial_op(model,ic_bnd)
 
         op_list.append(op.detach().numpy())
         bc_list.append(bc.reshape(-1).detach().numpy())
@@ -231,14 +211,10 @@ while t<1e5:
 
             if PLOT_SAVE:
                 fig = plt.figure(figsize=(15,8))
-                ax1 = fig.add_subplot( projection='3d')
-                ax1.plot_trisurf(grid[:, 0].detach().cpu().numpy().reshape(-1), 
-                            grid[:, 1].detach().cpu().numpy().reshape(-1),
-                            model(grid).detach().cpu().numpy().reshape(-1),
-                            cmap=plt.cm.jet, linewidth=0.2, alpha=1)
-                ax1.set_xlabel("x1")
-                ax1.set_ylabel("x2")
-                plt.savefig('disp_exp/plot_disp_{}.png'.format(t))
+                plt.plot(t_grid,model(grid).reshape(-1).detach().numpy())
+                #plt.set_xlabel("t")
+                #plt.set_ylabel("y")
+                plt.savefig('disp_exp_ODE/plot_disp_{}.png'.format(t))
         t+=1
 
 
