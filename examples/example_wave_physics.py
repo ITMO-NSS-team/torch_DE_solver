32c504da4be782550e560d41ae7bcbcc9948cf6a
SuperSashka <heretik.unlimited@gmail.com>
2023-07-25 23:37:16 +0300

    Example fix

--- examples/example_wave_physics.py
+++ examples/example_wave_physics.py
@@ -190,7 +190,7 @@ def wave_experiment(grid_res, CACHE):
         os.mkdir(img_dir)
 
     model = Solver(grid, equation, model, 'NN').solve(lambda_bound=100, verbose=1, learning_rate=1e-4,
-                                                      eps=1e-6, tmin=1000, tmax=1e5, use_cache=True, cache_verbose=True,
+                                                      eps=1e-4, tmin=1000, tmax=1e5, use_cache=True, cache_verbose=True,
                                                       save_always=True, print_every=None,
                                                       model_randomize_parameter=1e-6,
                                                       optimizer_mode='Adam', no_improvement_patience=1000,
