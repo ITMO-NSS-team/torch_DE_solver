c1f31e739eccdecb6156a44f5b3f40dabe2ce39f
dimerf99 <93322314+dimerf99@users.noreply.github.com>
2024-09-27 19:39:37 +0300

    Update plot.py
    Update callbacks plot file

--- tedeous/callbacks/plot.py
+++ tedeous/callbacks/plot.py
@@ -1,13 +1,15 @@
 import os
 import datetime
-from typing import Union
+from typing import Union, List
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from scipy.interpolate import griddata
 import torch
 from tedeous.callbacks.callback import Callback
 
 
+# Based version
 class Plots(Callback):
     """Class for ploting solutions."""
 
@@ -15,7 +17,8 @@ class Plots(Callback):
                  print_every: Union[int, None] = 500,
                  save_every: Union[int, None] = 500,
                  title: str = None,
-                 img_dir: str = None):
+                 img_dir: str = None,
+                 t_values: list = None):
         """
         Args:
             print_every (Union[int, None], optional): print plots after every *print_every* steps. Defaults to 500.
@@ -28,6 +31,7 @@ class Plots(Callback):
         self.save_every = save_every if save_every is not None else 0.1
         self.title = title
         self.img_dir = img_dir
+        self.t_values = t_values
 
     def _print_nn(self):
         """
@@ -56,6 +60,7 @@ class Plots(Callback):
 
         nparams = self.grid.shape[1]
         fig = plt.figure(figsize=(15, 8))
+
         for i in range(nvars_model):
             if nparams == 1:
                 ax1 = fig.add_subplot(1, nvars_model, i + 1)
@@ -75,6 +80,35 @@ class Plots(Callback):
                                  cmap=cm.jet, linewidth=0.2, alpha=1)
                 ax1.set_xlabel("x1")
                 ax1.set_ylabel("x2")
+
+    def _plot_solution_2d(self, t_values: List[float]):
+        """
+        Solution plot for 2D PDE u(x, y, t) at multiple time points.
+
+        Args:
+            t_values (List[float]): List of time points to plot the solution for.
+        """
+
+        grid_x = self.grid[:, 0].detach().cpu().numpy()
+        grid_y = self.grid[:, 1].detach().cpu().numpy()
+
+        for t in t_values:
+            time_mask = self.grid[:, 2] == t
+            z_values = self.net(self.grid[time_mask]).detach().cpu().numpy().flatten()
+
+            fig = plt.figure(figsize=(15, 8))
+            ax = fig.add_subplot(111, projection='3d')
+
+            ax.plot_trisurf(grid_x[time_mask], grid_y[time_mask], z_values, cmap=cm.jet, linewidth=0.2, alpha=1)
+            ax.set_title(f'Solution u(x, y) at t = {t}')
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            ax.set_zlabel("u(x, y)")
+
+            if self.img_dir:
+                directory = self._dir_path(self.img_dir, suffix=f"_t{t}")
+                plt.savefig(directory)
+            plt.close(fig)
 
     def _print_mat(self):
         """
@@ -103,16 +137,16 @@ class Plots(Callback):
             ax1.set_xlabel("x1")
             ax1.set_ylabel("x2")
 
-    def _dir_path(self, save_dir: str) -> str:
+    def _dir_path(self, save_dir: str, suffix: str = "") -> str:
         """ Path for save figures.
 
         Args:
             save_dir (str): directory where saves in
+            suffix (str): suffix for file name
 
         Returns:
             str: directory where saves in
         """
-
         if save_dir is None:
             try:
                 img_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'img')
@@ -123,36 +157,58 @@ class Plots(Callback):
             if not os.path.isdir(img_dir):
                 os.mkdir(img_dir)
             directory = os.path.abspath(os.path.join(img_dir,
-                                                     str(datetime.datetime.now().timestamp()) + '.png'))
+                                                     str(datetime.datetime.now().timestamp()) + suffix + '.png'))
         else:
             if not os.path.isdir(save_dir):
                 os.mkdir(save_dir)
             directory = os.path.join(save_dir,
-                                     str(datetime.datetime.now().timestamp()) + '.png')
+                                     str(datetime.datetime.now().timestamp()) + suffix + '.png')
         return directory
 
-    def solution_print(
-            self):
+    def solution_print(self):
         """ printing or saving figures.
         """
+
         print_flag = self.model.t % self.print_every == 0
         save_flag = self.model.t % self.save_every == 0
 
         if print_flag or save_flag:
             self.net = self.model.net
             self.grid = self.model.solution_cls.grid
+
             if self.model.mode == 'mat':
                 self._print_mat()
             else:
-                self._print_nn()
-            if save_flag:
-                directory = self._dir_path(self.img_dir)
-                plt.savefig(directory)
-            if print_flag:
-                plt.show()
-            plt.close()
+                if self.grid.shape[1] > 2:
+                    if self.t_values is None:
+                        self.t_values = torch.unique(self.grid[:, 2]).detach().cpu().numpy()
+
+                    selected_t_values = [self.t_values[1],
+                                         self.t_values[len(self.t_values) // 4],
+                                         self.t_values[len(self.t_values) // 2]]
+                    self._plot_solution_2d(selected_t_values)
+                else:
+                    self._print_nn()
+
+                    if save_flag:
+                        directory = self._dir_path(self.img_dir)
+                        plt.savefig(directory)
+                    if print_flag:
+                        plt.show()
+                    plt.close()
 
     def on_epoch_end(self, logs=None):
         self.solution_print()
+
+
+
+
+
+
+
+
+
+
+
 
 
