f1712ec08dec5627fbb52409607b6f85b607e86d
dimerf99 <93322314+dimerf99@users.noreply.github.com>
2024-12-17 18:29:00 +0300

    Update example_poisson_2d_classic.py
    Correcting code

--- examples/examples_PINNacle/example_poisson/example_poisson_2d_classic.py
+++ examples/examples_PINNacle/example_poisson/example_poisson_2d_classic.py
@@ -124,7 +124,7 @@ def poisson_2d_classic_experiment(grid_res):
 
     optimizer = Optimizer('Adam', {'lr': 1e-3})
 
-    model.train(optimizer, 5e6, save_model=True, callbacks=[cb_cache, cb_es, cb_plots])
+    model.train(optimizer, 5e5, save_model=True, callbacks=[cb_cache, cb_es, cb_plots])
 
     end = time.time()
 
@@ -136,8 +136,13 @@ def poisson_2d_classic_experiment(grid_res):
 
     error_rmse = torch.sqrt(torch.mean((exact - net_predicted) ** 2))
 
-    exp_dict_list.append({'grid_res': grid_res, 'time': end - start, 'RMSE': error_rmse.detach().cpu().numpy(),
-                          'type': 'wave_eqn_physical', 'cache': True})
+    exp_dict_list.append({
+        'grid_res': grid_res,
+        'time': end - start,
+        'RMSE': error_rmse.detach().cpu().numpy(),
+        'type': 'poisson_2d_classic',
+        'cache': True
+    })
 
     print('Time taken {}= {}'.format(grid_res, end - start))
     print('RMSE {}= {}'.format(grid_res, error_rmse))
@@ -158,6 +163,6 @@ import pandas as pd
 
 exp_dict_list_flatten = [item for sublist in exp_dict_list for item in sublist]
 df = pd.DataFrame(exp_dict_list_flatten)
-df.to_csv('examples/benchmarking_data/wave_experiment_physical_30_300_cache={}.csv'.format(str(True)))
+df.to_csv('examples/benchmarking_data/poisson_2d_classic_experiment_30_300_cache={}.csv'.format(str(True)))
 
 
