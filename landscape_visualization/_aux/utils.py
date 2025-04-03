78e1bf5e4d53d4f69649b1d4154e06ac05d6343d
dimerf99 <93322314+dimerf99@users.noreply.github.com>
2025-04-03 19:26:42 +0300

    Update utils.py
    Correcting code

--- landscape_visualization/_aux/utils.py
+++ landscape_visualization/_aux/utils.py
@@ -269,8 +269,8 @@ def get_density_CKA(matrix):
     # HKH: n x n
 
     # norms = np.sqrt(np.diag(HKH))
-    # norms = np.sqrt(np.clip(np.diag(HKH), a_min=0, a_max=None))
-    norms = np.sqrt(np.abs(np.diag(HKH)))
+    norms = np.sqrt(np.clip(np.diag(HKH), a_min=0, a_max=None))
+    # norms = np.sqrt(np.abs(np.diag(HKH)))
     # norms: n
 
     outer_norms = np.outer(norms, norms)
