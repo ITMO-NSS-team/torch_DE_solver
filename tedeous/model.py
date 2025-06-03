6bf4bb616f0378cb97264b859052809ae37bb747
dimerf99 <93322314+dimerf99@users.noreply.github.com>
2025-06-03 13:51:34 +0300

    Rollback model.py
    All changes related to the optimizer chain and NNCG must be in a separate PR.

--- tedeous/model.py
+++ tedeous/model.py
@@ -43,8 +43,6 @@ class Model():
         self.equation = equation
         self.conditions = conditions
 
-        self.grid = None
-
         self._check = None
         temp_dir = tempfile.gettempdir()
         folder_path = os.path.join(temp_dir, 'tedeous_cache/')
@@ -95,25 +93,22 @@ class Model():
         self.weak_form = weak_form
         self.removed_domains = removed_domains
 
-        # grid = self.domain.build(mode=mode, removed_domains=removed_domains)
-        # dtype = grid.dtype
-
-        self.grid = self.domain.build(mode=mode, removed_domains=removed_domains)
-        dtype = self.grid.dtype
+        grid = self.domain.build(mode=mode, removed_domains=removed_domains)
+        dtype = grid.dtype
 
         self.net.to(dtype)
         variable_dict = self.domain.variable_dict
         operator = self.equation.equation_lst
         bconds = self.conditions.build(variable_dict)
 
-        self.equation_cls = Operator_bcond_preproc(self.grid, operator, bconds, h=h, inner_order=inner_order,
+        self.equation_cls = Operator_bcond_preproc(grid, operator, bconds, h=h, inner_order=inner_order,
                                                    boundary_order=boundary_order).set_strategy(mode)
 
         if self.batch_size != None:
-            if len(self.grid) < self.batch_size:
+            if len(grid) < self.batch_size:
                 self.batch_size = None
 
-        self.solution_cls = Solution(self.grid, self.equation_cls, self.net, mode, weak_form,
+        self.solution_cls = Solution(grid, self.equation_cls, self.net, mode, weak_form,
                                      lambda_operator, lambda_bound, tol, derivative_points,
                                      batch_size=self.batch_size)
 
@@ -137,7 +132,7 @@ class Model():
                 save_model_nn(self._save_dir, model=self.net, name=model_name)
 
     def train(self,
-              optimizer: Union[Optimizer, list, dict],
+              optimizer: Optimizer,
               epochs: int,
               info_string_every: Union[int, None] = None,
               mixed_precision: bool = False,
@@ -147,7 +142,7 @@ class Model():
         """ train model.
 
         Args:
-            optimizer (Union[Optimizer, list, dict]): the object of Optimizer class or dict, or list
+            optimizer (Optimizer): the object of Optimizer class
             epochs (int): number of epoch for training.
             info_string_every (Union[int, None], optional): print loss state after *info_string_every* epoch. Defaults to None.
             mixed_precision (bool, optional): apply mixed precision for calculation. Defaults to False.
@@ -157,85 +152,53 @@ class Model():
         """
 
         self.t = 1
-        self.saved_models = []
-
         self.stop_training = False
+
         callbacks = CallbackList(callbacks=callbacks, model=self)
+
         callbacks.on_train_begin()
 
         self.net = self.solution_cls.model
 
-        # Initialize min_loss
+        self.optimizer = optimizer.optimizer_choice(self.mode, self.net)
+
+        closure = Closure(mixed_precision, self).get_closure(optimizer.optimizer)
+
         self.min_loss, _ = self.solution_cls.evaluate()
+
         self.cur_loss = self.min_loss
 
-        print('[{}] initial (min) loss is {}'.format(datetime.datetime.now(), self.min_loss.item()))
+        print('[{}] initial (min) loss is {}'.format(
+                datetime.datetime.now(), self.min_loss.item()))
 
-        def execute_training_phase(epochs, new_graph_flag=None):
-            while self.t < epochs and not self.stop_training:
-                callbacks.on_epoch_begin()
-                self.optimizer.zero_grad()
+        while self.t < epochs and self.stop_training is False:
+            callbacks.on_epoch_begin()
+            self.optimizer.zero_grad()
 
-                # this fellow should be in NNCG closure, but since it calls closure many times,
-                # it updates several time, which casuses instability
+            #this fellow should be in NNCG closure, but since it calls closure many times, it updates several time, which casuses instability
+            if optimizer.optimizer == 'NNCG' and ((self.t-1) % optimizer.params['precond_update_frequency'] == 0):
+                grads = self.optimizer.gradient(self.cur_loss)
+                grads = torch.where(grads != grads, torch.zeros_like(grads), grads)
+                self.optimizer.update_preconditioner(grads)
 
-                if optimizer.optimizer == 'NNCG' and ((self.t - 1) % optimizer.params['precond_update_frequency'] == 0):
-                    if new_graph_flag:
-                        with torch.autocast(
-                                device_type=device_type(),
-                                dtype=self.grid.dtype,
-                                enabled=mixed_precision
-                        ):
-                            loss, loss_normalized = self.solution_cls.evaluate()
-                        grads = self.optimizer.gradient(loss)
-                        new_graph_flag = None
-                    else:
-                        grads = self.optimizer.gradient(self.cur_loss)
 
-                    grads = torch.where(grads != grads, torch.zeros_like(grads), grads)
-                    print('here t={} and freq={}'.format(self.t - 1, optimizer.params['precond_update_frequency']))
-                    self.optimizer.update_preconditioner(grads)
+            iter_count = 1 if self.batch_size is None else self.solution_cls.operator.n_batches
+            for _ in range(iter_count): # if batch mod then iter until end of batches else only once
+                if device_type() == 'cuda' and mixed_precision:
+                    closure()
+                else:
+                    self.optimizer.step(closure)
+                if optimizer.gamma is not None and self.t % optimizer.decay_every == 0:
+                    optimizer.scheduler.step()
+            callbacks.on_epoch_end()
 
-                iter_count = 1 if self.batch_size is None else self.solution_cls.operator.n_batches
-                for _ in range(iter_count):  # if batch mod then iter until end of batches else only once
-                    if device_type() == 'cuda' and mixed_precision:
-                        closure()
-                    else:
-                        self.optimizer.step(closure)
-                    if optimizer.gamma is not None and self.t % optimizer.decay_every == 0:
-                        optimizer.scheduler.step()
-
-                loss = self.cur_loss.item() if isinstance(self.cur_loss, torch.Tensor) else self.cur_loss
-
-                callbacks.on_epoch_end()
-                self.t += 1
-
-                if info_string_every is not None and self.t % info_string_every == 0:
-                    print(f'[{datetime.datetime.now()}] Step = {self.t}, loss = {loss:.6f}.')
-
-        if isinstance(optimizer, list):
-            optimizers_chain = optimizer.copy()
-            for i_opt in range(len(optimizers_chain)):
-                opt_name = optimizers_chain[i_opt]['name']
-                opt_params = optimizers_chain[i_opt]['params']
-                opt_epochs = optimizers_chain[i_opt]['epochs']
-                optimizer = Optimizer(opt_name, opt_params)
-
-                self.optimizer = optimizer.optimizer_choice(self.mode, self.net)
-
-                closure = Closure(mixed_precision, self).get_closure(optimizer.optimizer)
-                self.t = 1
-
-                print(f'\n[{datetime.datetime.now()}] Using optimizer: {opt_name} for {opt_epochs} epochs.')
-                execute_training_phase(opt_epochs, new_graph_flag=i_opt)
-                print(f'[{datetime.datetime.now()}] Finished optimizer {opt_name}.')
-
-        elif isinstance(optimizer, Optimizer):
-            self.optimizer = optimizer.optimizer_choice(self.mode, self.net)
-            closure = Closure(mixed_precision, self).get_closure(optimizer.optimizer)
-            execute_training_phase(epochs)
+            self.t += 1
+            if info_string_every is not None:
+                if self.t % info_string_every == 0:
+                    loss = self.cur_loss.item() if isinstance(self.cur_loss, torch.Tensor) else self.cur_loss
+                    info = '[{}] Step = {} loss = {:.6f}.'.format(datetime.datetime.now(),self.t, loss)
+                    print(info)
 
         callbacks.on_train_end()
 
         self._model_save(save_model, model_name)
-        
