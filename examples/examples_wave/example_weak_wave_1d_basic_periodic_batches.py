396f0012d8c635007fe9b08ba8f201d87d62b22e
dimerf99 <93322314+dimerf99@users.noreply.github.com>
2025-06-03 13:15:00 +0300

    Correct wave_1d_basic_periodic_batches example

--- examples/examples_wave/example_weak_wave_1d_basic_periodic_batches.py
+++ examples/examples_wave/example_weak_wave_1d_basic_periodic_batches.py
@@ -5,7 +5,8 @@ import os
 import time
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../examples_wave')))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(project_root)
 
 from tedeous.data import Domain, Conditions, Equation
 from tedeous.model import Model
