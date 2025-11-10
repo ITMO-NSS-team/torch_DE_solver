5d1ed3cfe8c587cf18c55061dcf64b9079d7b03e
dimerf99 <93322314+dimerf99@users.noreply.github.com>
2025-11-10 15:24:11 +0300

    Update example_navier_stokes_2d_TG_vortex.py

--- examples/examples_PINNacle/example_navier_stokes_PINNacle/example_navier_stokes_2d_TG_vortex.py
+++ examples/examples_PINNacle/example_navier_stokes_PINNacle/example_navier_stokes_2d_TG_vortex.py
@@ -1,228 +1,347 @@
 import torch
 import os
 import sys
+import time
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../examples_navier_stokes')))
 
 from tedeous.data import Domain, Conditions, Equation
 from tedeous.model import Model
-from tedeous.callbacks import early_stopping, plot
+from tedeous.callbacks import early_stopping, cache, plot
 from tedeous.optimizers.optimizer import Optimizer
 from tedeous.device import solver_device
 
 solver_device('gpu')
 
-k = 1
 ro = 1
 mu = 2 * torch.pi / 100
 
-x_min, x_max = 0, 2 * torch.pi
-y_min, y_max = 0, 2 * torch.pi
-t_max = 2
-grid_res = 10
 
-domain = Domain()
-domain.variable('x', [x_min, x_max], grid_res)
-domain.variable('y', [y_min, y_max], grid_res)
-domain.variable('t', [0, t_max], grid_res)
+def exact_func_u(grid):
+    x, y, t = grid[:, 0], grid[:, 1], grid[:, 2]
+    sln = -torch.cos(x[:, None]) * torch.sin(y[:, None]) * torch.exp(-2 * mu * t[:, None])
+    return sln
 
-x = domain.variable_dict['x']
-y = domain.variable_dict['y']
-t = domain.variable_dict['t']
 
-boundaries = Conditions()
+def exact_func_v(grid):
+    x, y, t = grid[:, 0], grid[:, 1], grid[:, 2]
+    sln = torch.sin(x[:, None]) * torch.cos(y[:, None]) * torch.exp(-2 * mu * t[:, None])
+    return sln
 
-# Initial conditions (TG vortex) ###################################################################################
 
-boundaries.dirichlet({'x': [x_min, x_max], 'y': [y_min, y_max], 't': 0},
-                     value=torch.sin(k * x) * torch.cos(k * y),
-                     var=0)
-boundaries.dirichlet({'x': [x_min, x_max], 'y': [y_min, y_max], 't': 0},
-                     value=-torch.cos(k * x) * torch.sin(k * y),
-                     var=1)
-boundaries.dirichlet({'x': [x_min, x_max], 'y': [y_min, y_max], 't': 0},
-                     value=-ro / 4 * (torch.cos(2 * k * x) + torch.cos(2 * k * y)),
-                     var=2)
+def exact_func_p(grid):
+    x, y, t = grid[:, 0], grid[:, 1], grid[:, 2]
+    sln = -1 / 4 * (torch.cos(2 * x[:, None]) + torch.cos(2 * y[:, None])) * torch.exp(-4 * mu * t[:, None])
+    return sln
 
-# Boundary conditions for u-function ###############################################################################
 
-# u(x_min, y, t) = sin(x)
-boundaries.dirichlet({'x': x_min, 'y': [y_min, y_max], 't': [0, t_max]}, value=torch.sin(x), var=0)
-# u(x_max, y, t) = -sin(x)
-boundaries.dirichlet({'x': x_max, 'y': [y_min, y_max], 't': [0, t_max]}, value=-torch.sin(x), var=0)
-# u(x, y_min, t) = 0
-boundaries.dirichlet({'x': [x_min, x_max], 'y': y_min, 't': [0, t_max]}, value=0, var=0)
-# u(x, y_max, t) = 0
-boundaries.dirichlet({'x': [x_min, x_max], 'y': y_max, 't': [0, t_max]}, value=0, var=0)
+def navier_stokes_2d_TG_vortex_experiment(grid_res):
+    exp_dict_list_u, exp_dict_list_v, exp_dict_list_p = [], [], []
 
-# Boundary conditions for v-function ###############################################################################
+    x_min, x_max = 0, 2 * torch.pi
+    y_min, y_max = 0, 2 * torch.pi
+    t_max = 2
 
-# v(x_min, y, t) = 0
-boundaries.dirichlet({'x': x_min, 'y': [y_min, y_max], 't': [0, t_max]}, value=0, var=1)
-# v(x_max, y, t) = 0
-boundaries.dirichlet({'x': x_max, 'y': [y_min, y_max], 't': [0, t_max]}, value=0, var=1)
-# v(x, y_min, t) = -sin(y)
-boundaries.dirichlet({'x': [x_min, x_max], 'y': y_min, 't': [0, t_max]}, value=-torch.sin(y), var=1)
-# v(x, y_max, t) = sin(y)
-boundaries.dirichlet({'x': [x_min, x_max], 'y': y_max, 't': [0, t_max]}, value=torch.sin(y), var=1)
+    pde_dim_in = 3
+    pde_dim_out = 3
 
-# Boundary conditions for p-function ###############################################################################
+    domain = Domain()
+    domain.variable('x', [x_min, x_max], grid_res)
+    domain.variable('y', [y_min, y_max], grid_res)
+    domain.variable('t', [0, t_max], 11)
 
-# p(x, y_max, t) = 0
-boundaries.dirichlet({'x': x_min, 'y': [y_min, y_max], 't': [0, t_max]}, value=0, var=2)
-# v(x, y_max, t) = 0
-boundaries.dirichlet({'x': x_max, 'y': [y_min, y_max], 't': [0, t_max]}, value=0, var=2)
+    boundaries = Conditions()
 
-equation = Equation()
+    # Initial conditions ###############################################################################################
 
-# operator 1: # operator: u_x + v_y = 0
-NS_1 = {
-    'du/dx':
-        {
-            'coeff': 1,
-            'term': [0],
-            'pow': 1,
-            'var': 0
-        },
-    'dv/dy':
-        {
-            'coeff': 1,
-            'term': [1],
-            'pow': 1,
-            'var': 1
-        }
-}
+    boundaries.dirichlet({'x': [x_min, x_max], 'y': [y_min, y_max], 't': 0},
+                         value=lambda grid: -torch.cos(grid[:, 0]) * torch.sin(grid[:, 1]),
+                         var=0)
+    boundaries.dirichlet({'x': [x_min, x_max], 'y': [y_min, y_max], 't': 0},
+                         value=lambda grid: torch.sin(grid[:, 0]) * torch.cos(grid[:, 1]),
+                         var=1)
+    boundaries.dirichlet({'x': [x_min, x_max], 'y': [y_min, y_max], 't': 0},
+                         value=lambda grid: -1 / 4 * (torch.cos(2 * grid[:, 0]) + torch.sin(2 * grid[:, 1])),
+                         var=2)
 
-# operator 2: u * u_x + v * u_y + 1 / ro * p_x - mu * (u_xx + u_yy) = 0
-NS_2 = {
-    'u * du/dx':
-        {
-            'coeff': 1,
-            'term': [[None], [0]],
-            'pow': [1, 1],
-            'var': [0, 0]
-        },
-    'v * du/dy':
-        {
-            'coeff': 1,
-            'term': [[None], [1]],
-            'pow': [1, 1],
-            'var': [1, 0]
-        },
-    '1/ro * dp/dx':
-        {
-            'coeff': 1 / ro,
-            'term': [0],
-            'pow': 1,
-            'var': 2
-        },
-    '-mu * d2u/dx2':
-        {
-            'coeff': -mu,
-            'term': [0, 0],
-            'pow': 1,
-            'var': 0
-        },
-    '-mu * d2u/dy2':
-        {
-            'coeff': -mu,
-            'term': [1, 1],
-            'pow': 1,
-            'var': 0
-        }
-}
+    # Boundary conditions ##############################################################################################
 
-# operator 3: u * v_x + v * v_y + 1 / ro * p_y - mu * (v_xx + v_yy) = 0
-NS_3 = {
-    'u * dv/dx':
-        {
-            'coeff': 1,
-            'term': [[None], [0]],
-            'pow': [1, 1],
-            'var': [0, 1]
-        },
-    'v * dv/dy':
-        {
-            'coeff': 1,
-            'term': [[None], [1]],
-            'pow': [1, 1],
-            'var': [1, 1]
-        },
-    '1/ro * dp/dy':
-        {
-            'coeff': 1 / ro,
-            'term': [1],
-            'pow': 1,
-            'var': 2
-        },
-    '-mu * d2v/dx2':
-        {
-            'coeff': -mu,
-            'term': [0, 0],
-            'pow': 1,
-            'var': 1
-        },
-    '-mu * d2v/dy2':
-        {
-            'coeff': -mu,
-            'term': [1, 1],
-            'pow': 1,
-            'var': 1
-        }
-}
+    # u-function
 
-equation.add(NS_1)
-equation.add(NS_2)
-equation.add(NS_3)
+    # u(x_min, y, t) = u(x_max, y, t)
+    boundaries.periodic([{'x': x_min, 'y': [y_min, y_max], 't': [0, t_max]},
+                         {'x': x_max, 'y': [y_min, y_max], 't': [0, t_max]}],
+                        var=0)
+    # u(x, y_min, t) = u(x, y_max, t)
+    boundaries.periodic([{'x': [x_min, x_max], 'y': y_min, 't': [0, t_max]},
+                         {'x': [x_min, x_max], 'y': y_max, 't': [0, t_max]}],
+                        var=0)
 
-neurons = 100
+    # v-function
 
-net = torch.nn.Sequential(
-    torch.nn.Linear(3, neurons),
-    torch.nn.Tanh(),
-    torch.nn.Linear(neurons, neurons),
-    torch.nn.Tanh(),
-    torch.nn.Linear(neurons, neurons),
-    torch.nn.Tanh(),
-    torch.nn.Linear(neurons, neurons),
-    torch.nn.Tanh(),
-    torch.nn.Linear(neurons, neurons),
-    torch.nn.Tanh(),
-    torch.nn.Linear(neurons, neurons),
-    torch.nn.Tanh(),
-    torch.nn.Linear(neurons, 3)
-)
+    # v(x_min, y, t) = v(x_max, y, t)
+    boundaries.periodic([{'x': x_min, 'y': [y_min, y_max], 't': [0, t_max]},
+                         {'x': x_max, 'y': [y_min, y_max], 't': [0, t_max]}],
+                        var=1)
+    # v(x, y_min, t) = v(x, y_max, t)
+    boundaries.periodic([{'x': [x_min, x_max], 'y': y_min, 't': [0, t_max]},
+                         {'x': [x_min, x_max], 'y': y_max, 't': [0, t_max]}],
+                        var=1)
 
-for m in net.modules():
-    if isinstance(m, torch.nn.Linear):
-        torch.nn.init.xavier_normal_(m.weight)
-        torch.nn.init.zeros_(m.bias)
+    # p-function
 
-model = Model(net, domain, equation, boundaries)
+    # p(x_min, y, t) = p(x_max, y, t)
+    boundaries.periodic([{'x': x_min, 'y': [y_min, y_max], 't': [0, t_max]},
+                         {'x': x_max, 'y': [y_min, y_max], 't': [0, t_max]}],
+                        var=2)
+    # p(x, y_min, t) = p(x, y_max, t)
+    boundaries.periodic([{'x': [x_min, x_max], 'y': y_min, 't': [0, t_max]},
+                         {'x': [x_min, x_max], 'y': y_max, 't': [0, t_max]}],
+                        var=2)
 
-model.compile('autograd', lambda_operator=1, lambda_bound=1000, tol=0.1)
+    equation = Equation()
 
-cb_es = early_stopping.EarlyStopping(eps=1e-5,
-                                     loss_window=100,
-                                     no_improvement_patience=1000,
-                                     patience=10,
-                                     info_string_every=5,
-                                     randomize_parameter=1e-5)
+    # Operator 1: u_x + v_y = 0
 
-img_dir = os.path.join(os.path.dirname(__file__), 'navier_stokes_2d_TG_vortex_img')
+    NS_1 = {
+        'du/dx':
+            {
+                'coeff': 1,
+                'term': [0],
+                'pow': 1,
+                'var': 0
+            },
+        'dv/dy':
+            {
+                'coeff': 1,
+                'term': [1],
+                'pow': 1,
+                'var': 1
+            }
+    }
 
-cb_plots = plot.Plots(save_every=100,
-                      print_every=None,
-                      img_dir=img_dir,
-                      img_dim='2d',
-                      scatter_flag=False,
-                      n_samples=4,
-                      plot_axes=[0, 1],
-                      fixed_axes=[2],
-                      var_transpose=False,
-                      figsize=(18, 6))
+    # Operator 2: u_t + u * u_x + v * u_y + 1 / ro * p_x - mu * (u_xx + u_yy) = 0
 
-optimizer = Optimizer('Adam', {'lr': 1e-5})
+    NS_2 = {
+        'du/dt':
+            {
+                'coeff': 1,
+                'term': [2],
+                'pow': 1,
+                'var': 0
+            },
+        'u * du/dx':
+            {
+                'coeff': 1,
+                'term': [[None], [0]],
+                'pow': [1, 1],
+                'var': [0, 0]
+            },
+        'v * du/dy':
+            {
+                'coeff': 1,
+                'term': [[None], [1]],
+                'pow': [1, 1],
+                'var': [1, 0]
+            },
+        '1/ro * dp/dx':
+            {
+                'coeff': 1 / ro,
+                'term': [0],
+                'pow': 1,
+                'var': 2
+            },
+        '-mu * d2u/dx2':
+            {
+                'coeff': -mu,
+                'term': [0, 0],
+                'pow': 1,
+                'var': 0
+            },
+        '-mu * d2u/dy2':
+            {
+                'coeff': -mu,
+                'term': [1, 1],
+                'pow': 1,
+                'var': 0
+            }
+    }
 
-model.train(optimizer, 1e6, save_model=True, callbacks=[cb_es, cb_plots])
+    # Operator 3: v_t + u * v_x + v * v_y + 1 / ro * p_y - mu * (v_xx + v_yy) = 0
+
+    NS_3 = {
+        'dv/dt':
+            {
+                'coeff': 1,
+                'term': [2],
+                'pow': 1,
+                'var': 1
+            },
+        'u * dv/dx':
+            {
+                'coeff': 1,
+                'term': [[None], [0]],
+                'pow': [1, 1],
+                'var': [0, 1]
+            },
+        'v * dv/dy':
+            {
+                'coeff': 1,
+                'term': [[None], [1]],
+                'pow': [1, 1],
+                'var': [1, 1]
+            },
+        '1/ro * dp/dy':
+            {
+                'coeff': 1 / ro,
+                'term': [1],
+                'pow': 1,
+                'var': 2
+            },
+        '-mu * d2v/dx2':
+            {
+                'coeff': -mu,
+                'term': [0, 0],
+                'pow': 1,
+                'var': 1
+            },
+        '-mu * d2v/dy2':
+            {
+                'coeff': -mu,
+                'term': [1, 1],
+                'pow': 1,
+                'var': 1
+            }
+    }
+
+    equation.add(NS_1)
+    equation.add(NS_2)
+    equation.add(NS_3)
+
+    neurons = 100
+
+    net = torch.nn.Sequential(
+        torch.nn.Linear(pde_dim_in, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, neurons),
+        torch.nn.Tanh(),
+        torch.nn.Linear(neurons, pde_dim_out)
+    )
+
+    for m in net.modules():
+        if isinstance(m, torch.nn.Linear):
+            torch.nn.init.xavier_normal_(m.weight)
+            torch.nn.init.zeros_(m.bias)
+
+    start = time.time()
+
+    model = Model(net, domain, equation, boundaries)
+
+    model.compile('autograd', lambda_operator=1, lambda_bound=100)
+
+    cb_cache = cache.Cache(cache_verbose=True, model_randomize_parameter=1e-5)
+
+    cb_es = early_stopping.EarlyStopping(eps=1e-5,
+                                         loss_window=100,
+                                         no_improvement_patience=1000,
+                                         patience=10,
+                                         info_string_every=10,
+                                         randomize_parameter=1e-5)
+
+    img_dir = os.path.join(os.path.dirname(__file__), 'navier_stokes_2d_TG_vortex_img')
+
+    cb_plots = plot.Plots(save_every=100,
+                          print_every=None,
+                          img_dir=img_dir,
+                          img_dim='2d',
+                          scatter_flag=False,
+                          plot_axes=[0, 1],
+                          fixed_axes=[2],
+                          n_samples=4,
+                          var_transpose=False)
+
+    optimizer = Optimizer('Adam', {'lr': 1e-4})
+
+    model.train(optimizer, 5e5, save_model=True, callbacks=[cb_cache, cb_es, cb_plots])
+
+    end = time.time()
+
+    grid = domain.build('NN').to('cuda')
+    net = net.to('cuda')
+
+    predicted_u, predicted_v, predicted_p = net(grid)[:, 0], net(grid)[:, 1], net(grid)[:, 2]
+    exact_u = exact_func_u(grid).reshape(-1, 1)
+    exact_v = exact_func_v(grid).reshape(-1, 1)
+    exact_p = exact_func_p(grid).reshape(-1, 1)
+
+    error_rmse_u = torch.sqrt(torch.mean((exact_u - predicted_u) ** 2))
+    error_rmse_v = torch.sqrt(torch.mean((exact_v - predicted_v) ** 2))
+    error_rmse_p = torch.sqrt(torch.mean((exact_p - predicted_p) ** 2))
+
+    exp_dict_list_u.append({
+        'grid_res': grid_res,
+        'time': end - start,
+        'RMSE_u_func': error_rmse_u.detach().cpu().numpy(),
+        'type': 'navier_stokes_2d_TG_vortex',
+        'cache': True
+    })
+    exp_dict_list_v.append({
+        'grid_res': grid_res,
+        'time': end - start,
+        'RMSE_v_func': error_rmse_v.detach().cpu().numpy(),
+        'type': 'navier_stokes_2d_TG_vortex',
+        'cache': True
+    })
+    exp_dict_list_p.append({
+        'grid_res': grid_res,
+        'time': end - start,
+        'RMSE_p_func': error_rmse_p.detach().cpu().numpy(),
+        'type': 'navier_stokes_2d_TG_vortex',
+        'cache': True
+    })
+
+    print('Time taken {}= {}'.format(grid_res, end - start))
+
+    print('RMSE_u_func {}= {}'.format(grid_res, error_rmse_u))
+    print('RMSE_v_func {}= {}'.format(grid_res, error_rmse_v))
+    print('RMSE_p_func {}= {}'.format(grid_res, error_rmse_p))
+
+    return exp_dict_list_u, exp_dict_list_v, exp_dict_list_p
+
+
+nruns = 10
+
+exp_dict_list_u, exp_dict_list_v, exp_dict_list_p = [], [], []
+
+for grid_res in range(100, 1001, 100):
+    for _ in range(nruns):
+        list_u, list_v, list_p = navier_stokes_2d_TG_vortex_experiment(grid_res)
+        exp_dict_list_u.append(list_u)
+        exp_dict_list_v.append(list_v)
+        exp_dict_list_p.append(list_p)
+
+import pandas as pd
+
+exp_dict_list_u_flatten = [item for sublist in exp_dict_list_u for item in sublist]
+exp_dict_list_v_flatten = [item for sublist in exp_dict_list_v for item in sublist]
+exp_dict_list_p_flatten = [item for sublist in exp_dict_list_p for item in sublist]
+
+df_u = pd.DataFrame(exp_dict_list_u_flatten)
+df_v = pd.DataFrame(exp_dict_list_v_flatten)
+df_p = pd.DataFrame(exp_dict_list_p_flatten)
+
+df_u.to_csv('examples/benchmarking_data/navier_stokes_2d_TG_vortex_experiment_100_1000_cache_u_func={}.csv'
+            .format(str(True)))
+df_v.to_csv('examples/benchmarking_data/navier_stokes_2d_TG_vortex_experiment_100_1000_cache_v_func={}.csv'
+            .format(str(True)))
+df_p.to_csv('examples/benchmarking_data/navier_stokes_2d_TG_vortex_experiment_100_1000_cache_p_func={}.csv'
+            .format(str(True)))
