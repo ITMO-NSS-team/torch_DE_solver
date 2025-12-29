bb9a84a714ddf95a90f641f8cdfe81b45dd3f5e9
dimerf99 <93322314+dimerf99@users.noreply.github.com>
2025-12-29 14:24:36 +0300

    discretization fix

--- examples/examples_PINNacle/example_diffusion_PINNacle/example_diffusion_1d.py
+++ examples/examples_PINNacle/example_diffusion_PINNacle/example_diffusion_1d.py
@@ -37,7 +37,7 @@ def diffusion_1d_experiment(grid_res):
 
     domain = Domain()
     domain.variable('x', [x_min, x_max], grid_res)
-    domain.variable('t', [0, t_max], 10)
+    domain.variable('t', [0, t_max], grid_res)
 
     boundaries = Conditions()
 
